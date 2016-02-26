5a58a4410e5a2fb34ebe77b191473aa37d0f7011
R-Tamaki <a8113128@gmail.com>
2016-02-26 12:51:24 +0900

    アカウント作成時のEmailアドレス入力の修正 電話番号に文字が入力されないように変更

--- src/main/java/tiscon1/form/AccountForm.java
+++ src/main/java/tiscon1/form/AccountForm.java
@@ -3,7 +3,8 @@ package tiscon1.form;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotEmpty;
 import tiscon1.model.Prefecture;
-
+import org.hibernate.validator.constraints.Email;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
@@ -13,15 +14,13 @@ import java.io.Serializable;
 @Data
 public class AccountForm implements Serializable {
     @Size(min = 1, max = 100)
-    @NotEmpty
     private String name;
 
-
     @NotEmpty
+    @Email
     private String email;
 
     @Size(min = 6)
-    @NotEmpty
     private String password;
 
     @Size(min = 1, max = 100)
@@ -34,6 +33,7 @@ public class AccountForm implements Serializable {
     private String company;
 
     @Size(min = 1, max = 100)
+    @Pattern(regexp = "[0-9]")
     private String telephoneNumber;
 
     private String prefectureCd;
--- src/main/java/tiscon1/form/AccountRegisterForm.java
+++ src/main/java/tiscon1/form/AccountRegisterForm.java
@@ -2,24 +2,27 @@ package tiscon1.form;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Email;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
  * @author kawasima
  */
+
 @Data
 public class AccountRegisterForm implements Serializable {
     @Size(min = 1, max = 100)
-    @NotEmpty
     private String name;
 
-
     @NotEmpty
+    @Email
     private String email;
 
     @Size(min = 6)
-    @NotEmpty
     private String password;
+
 }
